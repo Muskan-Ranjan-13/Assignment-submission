4ed48d9f11134a8b89a8d9530f099f36fd196200
Muskan-Ranjan-13 <89441028+Muskan-Ranjan-13@users.noreply.github.com>
2021-08-24 10:54:20 +0530

    ASSIGNMENT 1 DAY 1

---
+++ JS assign/Calculator.js
@@ -0,0 +1,13 @@
+//Calculator using javascript
+a=20
+b=10
+sum=a+b
+diff=a-b
+mul=a*b
+div=a/b
+rem=a%b
+console.log("Sum is: ",sum)
+console.log("Difference is: ",diff)
+console.log("Product is: ",mul)
+console.log("Division is: ",div)
+console.log("Remainder is: ",rem)
